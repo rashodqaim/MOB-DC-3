77f6004394d9fe98c33c0da6b76abddcda02687b
rashodqaim <rashodqaim@Rashods-MacBook-Pro.local>
2015-06-08 21:19:35 -0400

    Created Bio App
    Played with Segue

---
+++ Class Work/Lession 1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var welcome = "Welcome to Shift!"
+
+import UIKit
+
+let currentTimeAndDate = NSDate()
+var currentTemperature = 72.0
+println("Current temperature oustide is \(currentTemperature)")
+
+let oneHourFromNow = NSDate(timeIntervalSinceNow: 3600)
+currentTemperature = 74.0
+println("Current temperature oustide is \(currentTemperature)")
+
+let twoHoursFromNow = NSDate(timeIntervalSinceNow: 6400)
+currentTemperature = 76.0
+println("Current temperature oustide is \(currentTemperature)")
+
+let threeHoursFromNow = NSDate(timeIntervalSinceNow: 9600)
+currentTemperature = 80.0
+println("Current temperature oustide is \(currentTemperature)")
